82c526566094d099d68259ee02ee85fd14f0e530
Michael Gabriel <michael.gabriel@enpal.de>
2023-04-01 13:32:33 +0200

    test ci

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ st.title("Demo App")
 user_selection = st.sidebar.radio("Yes or no", ["yes", "no"])
 if st.button("click to see what user selected"):
     st.write(f"User selected {user_selection}")
+
+st.write("Test change!")
